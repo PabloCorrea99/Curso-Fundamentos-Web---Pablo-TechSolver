267e10fd451b9386f937882e2b94377b5cd89f26
PabloCorrea99 <38085662+PabloCorrea99@users.noreply.github.com>
2020-07-17 00:14:52 -0500

    Update settings.py

--- Django/forotech/forotech/settings.py
+++ Django/forotech/forotech/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7z05oazx2=abcr=-o(wbz$=!i495jrn%=w9j!14@q*=-7+)ytx'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
