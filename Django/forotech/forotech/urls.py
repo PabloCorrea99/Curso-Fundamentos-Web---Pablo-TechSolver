39f4d9a840a0edd9e3269e01fdb015d54af39167
PabloCorrea99 <pcorream2@eafit.edu.co>
2020-07-31 09:15:21 -0500

    Primera View y Template

--- Django/forotech/forotech/urls.py
+++ Django/forotech/forotech/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog import views as blog
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', blog.inicio, name='inicio'),
 ]
